63b1c982cff1325de9e3d4b53db989282b08d9b6
hadaroadams <hadaroadams1234@gmail.com>
2023-08-13 23:33:43 +0000

    completed color generator

--- src/App.js
+++ src/App.js
@@ -2,52 +2,68 @@ import './App.css';
 import Values from 'values.js';
 import Colors from './colors';
 import { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 const [color,setColor] = useState('')
 //const [color1,setColor1] = useState('')
 const [moreColor,setMoreColor] = useState(new Values('red').all(10))
-useEffect(()=>{
+
+const colorValue=(e)=>{
+  let value=e.target.value
+  setColor(value)
+}
+console.log(navigator.clipboard)
+
+const submitVAlue =(e)=>{
+  e.preventDefault()
   try{
     const colorList = new Values(color).all(10)
     setMoreColor(colorList)
   }catch(error){
-    //console.log(error)
+    if(color.trim()==''){
+      console.log('helo')
+      toast('Type or select a color',{
+        position:'top-center',
+        type:'error',
+        position:'top-center',
+        theme:'light',
+        }
+      )
+    }else{
+      toast('Invalid Color',{
+        position:'top-center',
+        type:'error',
+        position:'top-center',
+        theme:'light',
+      })
+    }
   }
-},[color])
-
-console.log(moreColor)
-
-const colorValue=(e)=>{
-  e.preventDefault()
-  let value=e.target.value
-  setColor(value)
-  console.log(color)
 }
+//const notify =()=>{toast('wow so easy')}
   return (
     <main className="h-screen">
       <section  className='w-full flex flex-col p-12 md:flex-row md:items-center'>
         <h1 className='text-3xl mb-5 mr-4'>Color Generator</h1>
-        <form className='flex items-center h-12' onSubmit={(e)=>{
-          e.preventDefault()
-          console.log(color)
-        }}>
+        <form className='flex items-center h-12' onSubmit={submitVAlue}>
           <input type='color' className='h-10 border-[blue] bg-white' onChange={colorValue} value={color}/>
           <input type='text' placeholder='#f15025'  onChange={colorValue} className='border-2 h-full w-[250px] rounded-[5px]' value={color}/>
-          <button className={`h-full w-28 rounded-md text-white`} style={{backgroundColor:`${color==''?'blue':color}`}}>Submit</button>
+          <button className={`h-full w-28 rounded-md text-white`} style={{backgroundColor:`${color.trim()==''?'blue':color}`}} >Submit</button>
         </form>
       </section>
       <section className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
         {moreColor.map((items, index)=>{
           const {weight,hex}=items
-          console.log(hex)
+          //console.log(hex)
           if(index<10){
-            return <Colors weight={weight} hex={hex} textColor={'black'}/>
+            return <Colors weight={weight} hex={hex} textColor={'black'} key={hex}/>
           }else{
-            return <Colors weight={weight} hex={hex} textColor={'white'}/>
+            return <Colors weight={weight} hex={hex} textColor={'white'} key={hex}/>
           }
         })}
       </section>
+      <ToastContainer/>
     </main>
   );
 }
--- src/colors.jsx
+++ src/colors.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify'
 
 function Colors({hex,weight,textColor}) {
+
+  const copy =()=>{
+    navigator.clipboard.writeText(`#${hex}`)
+    toast('Copied TO clipboard',{
+      position:'top-center',
+      theme:'light',
+      type:'success'
+    })
+  }
   return (
-    <article className={`h-[95px]`} style={{backgroundColor:`#${hex}`,color:`${textColor}`}} >
+    <article className={`h-[95px] hover:cursor-pointer`} style={{backgroundColor:`#${hex}`,color:`${textColor}`}} onClick={copy} >
       <div className='w-full h-full flex flex-col items-center justify-center'>
         <span>{weight}%</span>
         <span>#{hex}</span>
